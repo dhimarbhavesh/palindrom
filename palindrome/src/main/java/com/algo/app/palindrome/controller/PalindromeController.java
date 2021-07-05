925655214bf697e3d893fce6671b96e7f6f0a5aa
bhave <bhave@192.168.0.8>
2021-07-05 12:01:05 -0500

    changes for package name

--- palindrome/src/main/java/com/algo/app/palindrome/controller/PalindromeController.java
+++ palindrome/src/main/java/com/algo/app/palindrome/controller/PalindromeController.java
@@ -1,4 +1,4 @@
-package com.algo.app.palindrome.comtroller;
+package com.algo.app.palindrome.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
